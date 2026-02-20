b4d1d262d1bde5c368012fead71600fe9b620e2b
shivam <shivamtiwarisgbt@gmail.com>
2026-02-20 07:45:37 +0530

    delete products from HTML

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem)=>{
     });
 
      cartPageHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingItem.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -99,7 +99,9 @@ document.querySelectorAll('.js-delete-quantity-link')
   link.addEventListener('click', ()=>{
     const productID = link.dataset.productId;
     removeFromcart(productID);
-    console.log(cart);
+  const container = document.querySelector(`.js-cart-item-container-${productID}`);
+    
+    container.remove();
     
   });
 });
